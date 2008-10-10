99ff350380a92f5dc20284b9f5cac688738dccc7
chillu <ingo@silverstripe.com>
2008-10-10 12:14:43 +0000

    ENHANCEMENT Added language table to genericviews module

---
+++ lang/en_US.php
@@ -0,0 +1,11 @@
+<?php
+
+global $lang;
+
+$lang['en_US']['RecordController']['SAVESUCCESS'] = 'Saved record';
+$lang['en_US']['RecordController']['SAVESUCCESS'] = 'Saved record';
+$lang['en_US']['RecordController']['DELETESUCCESS'] = 'Successfully deleted record';
+$lang['en_US']['CollectionController.ss']['SEARCH'] = 'Search';
+$lang['en_US']['CollectionController.ss']['ADDNEWRECORD'] = 'Add new record';
+
+?>
