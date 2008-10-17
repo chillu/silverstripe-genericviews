71bddfe431b7a742915fd3003ac8802b42369da4
chillu <ingo@silverstripe.com>
2008-10-17 21:50:33 +0000

    MINOR Updated lang files for genericviews module

--- lang/en_US.php
+++ lang/en_US.php
@@ -2,12 +2,36 @@
 
 global $lang;
 
-$lang['en_US']['RecordController']['SAVESUCCESS'] = 'Saved record';
-$lang['en_US']['RecordController']['SAVE'] = 'save';
-$lang['en_US']['RecordController']['DELETE'] = 'Delete';
-$lang['en_US']['RecordController']['SAVESUCCESS'] = 'Saved record';
-$lang['en_US']['RecordController']['DELETESUCCESS'] = 'Successfully deleted record';
-$lang['en_US']['CollectionController.ss']['SEARCH'] = 'Search';
 $lang['en_US']['CollectionController.ss']['ADDNEWRECORD'] = 'Add new record';
+$lang['en_US']['CollectionController.ss']['SEARCH'] = 'Search';
+$lang['en_US']['CollectionController_Results.ss']['NEXT'] = 'Next';
+$lang['en_US']['CollectionController_Results.ss']['NORESULTSFOUND'] = 'No records found';
+$lang['en_US']['CollectionController_Results.ss']['PREV'] = 'Prev';
+$lang['en_US']['CollectionController_Results.ss']['RESULTS'] = 'Results';
+$lang['en_US']['CollectionPage']['GENERICVIEWTAB'] = 'Generic Views';
+$lang['en_US']['CollectionPage']['db_CollectionControllerClass'] = array(
+	'CollectionControllerClass',
+	50,
+	'Name of the object property, e.g. used for automatically generating forms'
+);
+$lang['en_US']['CollectionPage']['db_CollectionModelClass'] = array(
+	'CollectionModelClass',
+	50,
+	'Name of the object property, e.g. used for automatically generating forms'
+);
+$lang['en_US']['CollectionPage']['plural_name'] = array(
+	'',
+	50,
+	'Pural name of the object, used in dropdowns and to generally identify a collection of this object in the interface'
+);
+$lang['en_US']['CollectionPage']['singular_name'] = array(
+	'',
+	50,
+	'Singular name of the object, used in dropdowns and to generally identify a single object in the interface'
+);
+$lang['en_US']['RecordController']['DELETE'] = 'Delete';
+$lang['en_US']['RecordController']['DELETESUCCESS'] = 'Successfully deleted record';
+$lang['en_US']['RecordController']['SAVE'] = 'save';
+$lang['en_US']['RecordController']['SAVESUCCESS'] = 'Saved record';
 
 ?>
