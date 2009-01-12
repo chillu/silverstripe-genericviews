49f0476f1ee2bb98a63832f0e0edf2fcd5026345
chillu <ingo@silverstripe.com>
2009-01-12 05:02:04 +0000

    MINOR Updated language master tables

--- lang/en_US.php
+++ lang/en_US.php
@@ -9,23 +9,13 @@ $lang['en_US']['CollectionController_Results.ss']['NORESULTSFOUND'] = 'No record
 $lang['en_US']['CollectionController_Results.ss']['PREV'] = 'Prev';
 $lang['en_US']['CollectionController_Results.ss']['RESULTS'] = 'Results';
 $lang['en_US']['CollectionPage']['GENERICVIEWTAB'] = 'Generic Views';
-$lang['en_US']['CollectionPage']['db_CollectionControllerClass'] = array(
-	'CollectionControllerClass',
-	50,
-	'Name of the object property, e.g. used for automatically generating forms'
-);
-$lang['en_US']['CollectionPage']['db_CollectionModelClass'] = array(
-	'CollectionModelClass',
-	50,
-	'Name of the object property, e.g. used for automatically generating forms'
-);
-$lang['en_US']['CollectionPage']['plural_name'] = array(
-	'',
+$lang['en_US']['CollectionPage']['PLURALNAME'] = array(
+	'Collection Pags',
 	50,
 	'Pural name of the object, used in dropdowns and to generally identify a collection of this object in the interface'
 );
-$lang['en_US']['CollectionPage']['singular_name'] = array(
-	'',
+$lang['en_US']['CollectionPage']['SINGULARNAME'] = array(
+	'Collection Page',
 	50,
 	'Singular name of the object, used in dropdowns and to generally identify a single object in the interface'
 );
